3b2de16977a4cb44b0de87bd7998dfcc6e53dbdf
lofructose <39576232+lofructose@users.noreply.github.com>
2022-06-21 14:51:23 -0500

    Completed ProductMaximizer ; fixed file

--- Java/ProductMaximizer.java
+++ Java/ProductMaximizer.java
@@ -2,13 +2,16 @@
 // By Andres Reyes Jr.
 public class ProductMaximizer{
   public static void main(String[] args){
-    int[] input = new int[]{10, 2, 34, 1, 2, 400};
+    int[] input = new int[]{100, 99, 110, 2, 2, 40, 110, 20, 4};
     int max1 = 0;
     int max2 = 0;
     for (int cur : input){
       if (cur > max1){
         max2 = max1;
         max1 = cur;
+      }
+      else if (cur > max2){
+        max2 = cur;
       }
     }
     System.out.println("Max1: " + max1);
